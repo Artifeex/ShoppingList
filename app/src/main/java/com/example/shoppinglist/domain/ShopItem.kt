e82642e86b25743bba4435eabb0bbb5124a5a50d
YoungPinch <dreamerandrew17@gmail.com>
2023-07-22 11:58:27 +0300

    create data-layer package and realisation of repository

--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.example.shoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
-    var name: String,
-    var count: Int,
-    var active: Boolean
-)
+    val name: String,
+    val count: Int,
+    val active: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
